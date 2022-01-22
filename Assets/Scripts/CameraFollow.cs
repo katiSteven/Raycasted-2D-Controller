3338ccc6b715abf19dacbab08fdef5dc9374a4ad
Steven Emmanuel <ksteven925@gmail.com>
2022-01-23 02:03:11 +0530

    Camera follow
    Focus area box around the Player object which eases camera alignment based on player movement input.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Controller2D target;
+    public float vecticalOffset;
+    public float lookAheadDstX;
+    public float lookSmoothTimeX;
+    public float verticalSmoothTime;
     public Vector2 focusAreaSize;
 
-    public float vecticalOffset;
-
     FocusArea focusArea;
+
+    float currentLookAheadX;
+    float targetLookAheadX;
+    float lookAheadDirX;
+    float smoothLookVelocityX;
+    float smoothVelocityY;
+
+    bool lookAheadStopped;
 
     private void Start() {
         focusArea = new FocusArea(target.collider.bounds, focusAreaSize);
@@ -18,9 +28,26 @@ public class CameraFollow : MonoBehaviour
     private void LateUpdate() {
         focusArea.Update(target.collider.bounds);
 
-        //Vector2 focusPosition = focusArea.center + Vector2.up * vecticalOffset;
+        Vector2 focusPosition = focusArea.center + Vector2.up * vecticalOffset;
 
-        //transform.position = (Vector3)focusPosition + Vector3.forward * -10;
+        if (focusArea.velocity.x != 0) {
+            lookAheadDirX = Mathf.Sign(focusArea.velocity.x);
+            if (Mathf.Sign(target.playerInput.x) == Mathf.Sign(focusArea.velocity.x) && target.playerInput.x != 0) {
+                lookAheadStopped = false;
+                targetLookAheadX = lookAheadDirX * lookAheadDstX;
+            } else {
+                if (!lookAheadStopped) {
+                    lookAheadStopped = true;
+                    targetLookAheadX = currentLookAheadX + (lookAheadDirX * lookAheadDstX - currentLookAheadX) / 4f;
+                }
+            }
+        }
+
+        currentLookAheadX = Mathf.SmoothDamp(currentLookAheadX, targetLookAheadX, ref smoothLookVelocityX, lookSmoothTimeX);
+
+        focusPosition.y = Mathf.SmoothDamp(transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
+        focusPosition += Vector2.right * currentLookAheadX;
+        transform.position = (Vector3)focusPosition + Vector3.forward * -10;
     }
 
     private void OnDrawGizmos() {
